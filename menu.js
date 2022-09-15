6edd163be97d92425d47c6a1d19b1b24aa76cc65
somarib <yesomarib1@gmail.com>
2022-09-15 18:24:08 +0000

    Add javascript in menu.js to close and show  mobile menu, formarted insex.html make if fixed and formated in index.html

---
+++ menu.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector("#mbm-icon");
+
+btn.addEventListener("click", function () {
+  
+  document.getElementById("hide-show").style.display = "block";
+});
+
+document.querySelectorAll(".mbmenu-li-a").forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelector("#hide-show").style.display = "none";
+  });
+});
